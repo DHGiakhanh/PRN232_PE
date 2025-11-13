e6eb1149a344e6dbf3e8ea2eb28f99b2afe52249
DHGiakhanh <khanhdhgqe180127@fpt.edu.vn>
2025-11-13 14:54:17 +0700

    update deploy

--- backend/Program.cs
+++ backend/Program.cs
@@ -3,11 +3,16 @@ using PRM.API.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+var safeConnectionString = connectionString ?? "Host=localhost;Port=5432;Database=postsdb;Username=postgres;Password=example";
+
+builder.Services.AddDbContext<AppDbContext>(opt => 
+{
+    opt.UseNpgsql(safeConnectionString);
+});
+
 builder.Services.AddControllers();
-
-var conn = builder.Configuration.GetConnectionString("DefaultConnection") ?? "Host=localhost;Port=5432;Database=postsdb;Username=postgres;Password=example";
-builder.Services.AddDbContext<AppDbContext>(opt => opt.UseNpgsql(conn));
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
